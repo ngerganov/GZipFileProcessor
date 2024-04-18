d3647d8bd82fbf60870a1137b6ec53ef7c9e7df1
Nikita <nikita.gerganov@gmail.com>
2024-04-18 21:03:36 +0300

    fix: BlockQueue

--- GZipFileProcessor/ConcurrentBlockQueue.cs
+++ GZipFileProcessor/ConcurrentBlockQueue.cs
@@ -6,10 +6,16 @@ public class ConcurrentBlockQueue
 {
     private readonly ConcurrentDictionary<int, Block> _blocks = new ConcurrentDictionary<int, Block>();
     private readonly ManualResetEvent _mre = new ManualResetEvent(false);
-    private int _currentIndex = 0;
+    private volatile int _currentIndex = 0;
+    private bool _isCompleted = false;
 
     public void Add(Block block)
     {
+        if (_isCompleted)
+        {
+            throw new InvalidOperationException("Cannot add blocks to a completed queue.");
+        }
+        
         _blocks.TryAdd(block.Index, block);
         _mre.Set();
     }
@@ -17,21 +23,30 @@ public class ConcurrentBlockQueue
     public Block Next()
     {
         _mre.WaitOne();
-        while (_blocks.ContainsKey(_currentIndex))
+        Block block;
+        while (!_blocks.TryRemove(_currentIndex, out block))
         {
-            Block block;
+            if (_isCompleted)
+            {
+                throw new InvalidOperationException("Queue is completed.");
+            }
+            
             if (_blocks.TryRemove(_currentIndex, out block))
             {
-                _currentIndex++;
-                return block;
+                break;
             }
+            
+            _mre.Reset();
+            _mre.WaitOne();
         }
-
-        return null;
+        
+        Interlocked.Increment(ref _currentIndex);
+        return block;
     }
 
     public void Complete()
     {
+        _isCompleted = true;
         _mre.Set();
     }
 }
